e5e962e99fded76f0ed1e6a97c624b9b713095c3
znzinc01 <admin@znzinc.net>
2018-11-29 01:54:27 +0900

    Update ic606_homework2_automator.py

--- Homework2/ic606_homework2_automator.py
+++ Homework2/ic606_homework2_automator.py
@@ -4,13 +4,13 @@ file_list = ["hw2_trace_bzip2.out", "hw2_trace_mcf.out"]
 
 for i in [1, 2, 4, 8]:
     for j in [16, 32, 64, 128, 256, 512]:
-        for k in [j, j/2]:
+        for k in [j, int(j/2)]:
             for l in file_list:
                 print("Width:", i)
                 print("ROB_size", j)
                 print("reservation_size", k)
                 print(l)
-                ooo = hw2.coreHW2(8, 16, 8, "./HW2_workloads/" + l,
+                ooo = hw2.coreHW2(i, j, k, "./HW2_workloads/" + l,
                                   0, True, False)
                 ooo.run_simulation()
                 print("============================")
